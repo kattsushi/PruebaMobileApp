6b85d33056b87a950a6692b073535f2d0f8e2bd1
kattsushi <ndresdavidj@gmail.com>
2016-03-02 21:24:55 -0430

    correccion de bugs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,25 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, figServ) {
+.controller('DashCtrl', function($scope, figServ, $resource) {
 
   $scope.figuras = [{
+    id : 1,
     figura : "circulo",
     parm1 : "radio",
     parm2 : "",
     use2 : false,
     comentario :"Para cacular el circulo necesitamos el radio",
     pi : Math.PI,
-    area : function (a,b) {
-      if (a > 0 ){ var radio = Math.round(this.pi * Math.pow(a, 2));}
-      return radio;
+    radio : 0,
+    area : 0,
+    calArea : function (a,b) {
+      if (a > 0 ){ 
+        this.radio = a ;
+        this.area = Math.round(this.pi * Math.pow(a, 2));}
+         return this.area;
     }
   },{
+    id : 2,
     figura : "triangulo",
     parm1 : "base",
     parm2 : "altura",
@@ -21,49 +27,66 @@ angular.module('starter.controllers', [])
     comentario :"Para cacular el área del triangulo necesitamos la base y la altura",
     base : $scope.x ,
     altura : $scope.y,
-    area : function (a,b) {
-      if (a > 0 && b > 0) { var area = (a * b)/2;}
-      return area;
-    }
+    area : 0,
+    calArea : function (a,b) {
+        this.area = ( a * b )/2;
+      if (isNaN(this.area)) {
+        return 0;
+      }else {
+        this.base = a;
+        this.altura = b;
+        return this.area;
+      }
+    } 
   },{
+    id : 3,
     figura : "cuadrado",
     parm1 : "lado",
     parm2 : "",
     use2 : false,
     comentario :"Para cacular el área del cuadrado necesitamos uno de sus lados",
-    lado : $scope.x,
-    area : function (a,b) {
-      if (a > 0) { var area = Math.pow(a,2);}
-      return area;
+    lado : 0,
+    area : 0,
+    calArea : function (a,b) {
+      if (a > 0) {
+          this.lado = a; 
+          this.area = Math.pow(a,2);
+          }
+      return this.area;
     }
   },{
+    id : 4,
     figura : "rectangulo",
     parm1 : "base",
     parm2 : "altura",
     use2 : true,
     comentario :"Para cacular el área del triangulo necesitamos la base y la altura",
-    base : $scope.x ,
-    altura : $scope.y,
-    area : function (a,b) {
-      if (a > 0 && b > 0) { var area = (a * b);}
-      return area;
-    }
+    base : 0,
+    altura : 0,
+    area : 0,
+    calArea : function (a,b) { 
+         this.area = ( a * b );
+      if (isNaN(this.area)) {
+        return 0;
+      }else {
+        this.base = a;
+        this.altura = b;
+        return this.area;
+      }
+    }  
   }];
   
-  function index(i) {
-    figServ.index = i;
-    console.log(i);
+  $scope.enviar = function (id) {
+     console.log(id);
+      $scope.figuras[id];
+      console.log($scope.figuras[id - 1]);
   }
-  figServ.data = $scope.figuras;
+  
 
 })
 
 .controller('AccountCtrl', function($scope,figServ) {
   
-  $scope.enviar = function () {
-    
-      console.warn(figServ.data[figServ.index]);
-  }
   $scope.settings = {
     enableFriends: true
   };
